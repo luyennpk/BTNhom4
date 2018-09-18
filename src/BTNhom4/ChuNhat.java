e983852a3eff31ba7023f6dc1372233999d67aab
luyennpk <nguyen.phuc.kim.luyen@framgia.com>
2018-09-18 12:23:49 +0700

    push code

--- src/BTNhom4/ChuNhat.java
+++ src/BTNhom4/ChuNhat.java
@@ -1,9 +1,25 @@
 package BTNhom4;
 
 public class ChuNhat {
+	
+	private float chieuDai;
+	private float chieuRong;
 
-	public ChuNhat() {
-		// TODO Auto-generated constructor stub
+	public ChuNhat(float chieuDai,float chieuRong) {
+		this.chieuDai = chieuDai;
+		this.chieuRong = chieuRong;
+	}	
+
+	public float Tinh_Dien_Tich()
+	{
+		return chieuDai*chieuRong;
 	}
+	
+	public float Tinh_Chu_Vi()
+	
+	{
+		return (chieuDai + chieuRong)*2;
+	}
+
 
 }
---
+++ src/BTNhom4/HinhVuong.java
@@ -0,0 +1,12 @@
+package BTNhom4;
+
+public class HinhVuong extends ChuNhat{
+
+	public HinhVuong(float chieuDai, float chieuRong) {
+		super(chieuDai, chieuRong);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
