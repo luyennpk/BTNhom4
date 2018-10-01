42d33370be5611c09acedb9e14746a308ac57092
luyennpk <nguyen.phuc.kim.luyen@framgia.com>
2018-10-02 05:07:45 +0700

    Luyen lam alert

---
+++ src/BTNhom4/AlertLuyen.java
@@ -0,0 +1,36 @@
+package BTNhom4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AlertLuyen {
+
+	public AlertLuyen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\framgia\\Downloads\\geckodriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		
+		String baseURL = "https://www.webtoolnavi.com/demo/sweetalert2/";
+		driver.get(baseURL);
+		driver.manage().window().maximize();
+		WebElement tryMeBtn = driver.findElement(By.xpath("/html/body/section[2]/div[13]/button"));
+		tryMeBtn.click();
+		
+		
+		driver.switchTo().window(driver.getWindowHandle()).findElement(By.xpath("/html/body/div[2]/div[2]/button[1]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/button[1]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/button[1]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/button[1]")).click();
+		
+	}
+
+}
